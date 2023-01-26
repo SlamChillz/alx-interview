a337cb6615b29f262b40687299dcfa20bcff7e98
slamchillz <mendyslam@gmail.com>
2023-01-26 16:25:10 +0100

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+Defines a UTF-8 Validation function
+"""
+
+
+def validUTF8(data):
+    """
+    UTF-8 Validation
+    Args:
+        data (list[int]): an array of characters represented as 1byte int
+    Returns:
+        (True): if all characters in data are valid UTF-8 code point
+        (False): if one or more characters in data are invalid code point
+    """
+    return all(list(map(lambda x: x >= 0 and x <= 128, data)))
